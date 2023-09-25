37d9d149ce401ca236f1424f1d708595a83a40e0
syed shariq ali <shariq.ali@bitswits.com>
2023-09-25 00:07:28 -0700

    changes

--- src/app/component/sidebar/page.js
+++ src/app/component/sidebar/page.js
@@ -160,7 +160,7 @@ function page() {
                    >
                       <li
                         key={submenu.id}
-                        // onClick={() => router.push(item.link)}
+                        onClick={() => router.push(item.link)}
                         // href={item.link}
                         className={`text-sm text-[#8b8b8d] flex items-center gap-x-4 p-3 px-4 mt-2 ${!state_Open && "-ml-1"}  cursor-pointer 
                       hover:bg-white rounded-md`}
